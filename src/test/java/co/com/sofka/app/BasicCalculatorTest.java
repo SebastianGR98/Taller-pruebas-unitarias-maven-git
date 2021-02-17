73151d58700bc9578a0cef9aa7aaaef83f238c5b
sebastiangrajales1998 <sebastiangrajales1998@gmail.com>
2021-02-17 12:36:12 -0500

    Casos de prueba de BasicCalculator

---
+++ src/test/java/co/com/sofka/app/BasicCalculatorTest.java
@@ -0,0 +1,40 @@
+package co.com.sofka.app;
+
+import co.com.sofka.app.calculator.BasicCalculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class BasicCalculatorTest {
+    private final BasicCalculator basicCalculator = new BasicCalculator();
+
+    @Test
+    @DisplayName("Testing sum: 1+1=2")
+    void sum() {
+        // Arrange
+        Long number1 = 1L;
+        Long number2 = 1L;
+        Long expectedValue = 2L;
+
+        // Act
+        Long result = basicCalculator.sumar(number1, number2);
+
+        // Assert
+        Assertions.assertEquals(expectedValue, result);
+    }
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    void severalSums(Long first, Long second, Long expectedResult) {
+        Assertions.assertEquals(expectedResult, basicCalculator.sumar(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+}
