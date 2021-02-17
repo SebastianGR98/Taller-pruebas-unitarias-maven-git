b5e680a8327d655a6a5cf4c7eb08c7532da925dd
sebastiangrajales1998 <sebastiangrajales1998@gmail.com>
2021-02-17 11:46:00 -0500

    Se añadió la puerta de entra a la calculadora

---
+++ src/main/java/co/com/sofka/app/App.java
@@ -0,0 +1,32 @@
+package co.com.sofka.app;
+
+import co.com.sofka.app.calculator.BasicCalculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger( App.class );
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        logger.info("Enter number 1: ");
+        String textNumber1 = bufferedReader.readLine();
+
+        logger.info("Enter number 2: ");
+        String textNumber2 = bufferedReader.readLine();
+
+        Long number1 = Long.valueOf(textNumber1);
+        Long number2 = Long.valueOf(textNumber2);
+
+        BasicCalculator calculator = new BasicCalculator();
+        Long result = calculator.sumar(number1, number2);
+
+        logger.info("Resultado de {} + {} = {}", number1, number2, result);
+    }
+}
