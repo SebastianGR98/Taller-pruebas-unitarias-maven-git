4f9f68403205d76059e58113ab1a161c27d8f135
sebastiangrajales1998 <sebastiangrajales1998@gmail.com>
2021-02-18 14:50:04 -0500

    Se añadió TODO: Hacer todos los escenarios restantes para la versificación de pruebas

--- src/test/java/co/com/sofka/app/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/BasicCalculatorTest.java
@@ -37,4 +37,43 @@ class BasicCalculatorTest {
         Assertions.assertEquals(expectedResult, basicCalculator.sumar(first, second),
                 () -> first + " + " + second + " should equal " + expectedResult);
     }
+
+    @DisplayName("Testing several rests")
+    @ParameterizedTest(name = "{0} - {1} = {2}")
+    @CsvSource({
+            "0,    1,   -1",
+            "10,    2,   8",
+            "49,  51, -2",
+            "100,  -100, 200"
+    })
+    void severalRests(Long first, Long second, Long expectedResult) {
+        Assertions.assertEquals(expectedResult, basicCalculator.restar(first, second),
+                () -> first + " - " + second + " should equal " + expectedResult);
+    }
+
+    @DisplayName("Testing several mults")
+    @ParameterizedTest(name = "{0} * {1} = {2}")
+    @CsvSource({
+            "0,    1,   0",
+            "0,    2,   0",
+            "49,  51, 2499",
+            "100,  -100, -10000"
+    })
+    void severalMults(Long first, Long second, Long expectedResult) {
+        Assertions.assertEquals(expectedResult, basicCalculator.multiplicar(first, second),
+                () -> first + " * " + second + " should equal " + expectedResult);
+    }
+
+    @DisplayName("Testing several divss")
+    @ParameterizedTest(name = "{0} / {1} = {2}")
+    @CsvSource({
+            "0,    1,   0",
+            "1,    0,   0",
+            "-28,  -8, 3",
+            "100,  -100, -1"
+    })
+    void severalDivs(Long first, Long second, Long expectedResult) {
+        Assertions.assertEquals(expectedResult, basicCalculator.dividir(first, second),
+                () -> first + " / " + second + " should equal " + expectedResult);
+    }
 }
